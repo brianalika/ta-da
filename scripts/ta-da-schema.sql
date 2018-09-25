2c08e259899347dd546a33ee38ad5a5af49ccfc9
Brian Clark <brian.alika.clark@gmail.com>
2018-09-25 15:58:12 -0600

    Fixed Table Name in MySQL

--- scripts/ta-da-schema.sql
+++ scripts/ta-da-schema.sql
@@ -1,4 +1,5 @@
-CREATE TABLE top5000 (
+USE l75jwxdw89v7d4lm;
+CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(20) NOT NULL,
     password CHAR(60) NOT NULL,
